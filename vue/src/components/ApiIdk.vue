7962973b02786a68d2a359f8a7157492711016dc
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2024-04-03 09:16:56 -0400

    Update ApiIdk.vue

--- vue/src/components/ApiIdk.vue
+++ vue/src/components/ApiIdk.vue
@@ -7,7 +7,7 @@
 <script setup>
 
 </script>
-
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzZXJzZ2l2emhka3p0anNobmdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNTAyOTYsImV4cCI6MjAyNzcyNjI5Nn0.kMXmf_1epcmlVR_dxJwhSMZXey_SUzPC15zSeaIUcW0
 <style scoped>
 
 </style>

5ddc644bf178f2feafca647b09940a519ac170c8
dakotas17 <dakotas17@nycstudents.net>
2024-04-04 09:24:11 -0400

    awd

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -4,6 +4,18 @@
 
 <template>
   <main>
-    <TheWelcome />
+    <form> 
+    <h1 class="label-wrapper">HIIIIII</h1>
+    <label for="new-todo-input"
+    class="label_lg">what needs blah</label>
+    </h1>
+    <input type="text"
+    id="new-todo-input"
+    autocomplete="off"
+  </form>
   </main>
 </template>
+
+<style scoped>
+
+</style>

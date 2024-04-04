24cb3f356dc1d4b4d92453dd331316a5aac5ba65
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-04-04 09:23:51 -0400

    a

--- vue/src/lib/supabaseClient.js
+++ vue/src/lib/supabaseClient.js
@@ -1,3 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
 
   export const supabase = createClient('https://<project>.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzZXJzZ2l2emhka3p0anNobmdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNTAyOTYsImV4cCI6MjAyNzcyNjI5Nn0.kMXmf_1epcmlVR_dxJwhSMZXey_SUzPC15zSeaIUcW0')
+
+  async function signUpNewUser() {
+    const { data, error } = await supabase.auth.signUp({
+      email: 'example@email.com',
+      password: 'example-password',
+      options: {
+        emailRedirectTo: 'https://example.com/welcome',
+      },
+    })
+  }
+signUpNewUser();  

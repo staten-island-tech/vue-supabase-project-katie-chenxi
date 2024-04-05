ce0f73fe37a29011aa0851c8e5b84d147d5ed95c
dakotas17 <dakotas17@nycstudents.net>
2024-04-05 09:19:40 -0400

    sesfse

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,21 +1,34 @@
 <script setup lang="ts">
-
+function warn(message, event){
+  if (event){
+    event.preventDefault()
+  }
+  alert(message)
+}
 </script>
 
 <template>
   <main>
     <form> 
-    <h1 class="label-wrapper">HIIIIII</h1>
+    <h2 class="label-wrapper">
     <label for="new-todo-input"
-    class="label_lg">what needs blah</label>
-    </h1>
+    class="label_lg">user input maybe</label>
+    </h2>
     <input type="text"
     id="new-todo-input"
     autocomplete="off"
+    trim="label"
+    class="input_lg"/>
+    <button type="submit" class="btn btn_primary btn_lg" @click="warn('testing', $event)">Add</button>
+<!-- can alert if set password is too short -->
   </form>
   </main>
+
+
 </template>
 
-<style scoped>
-
+<style>
+h2{
+  background-color: pink;
+}
 </style>

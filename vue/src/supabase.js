b3e3ddbf8d0c90694c0eb0360965441cc4a6c8b4
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-04-09 09:09:54 -0400

    A

--- vue/src/supabase.js
+++ vue/src/supabase.js
@@ -4,6 +4,3 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export default function supabase() {
-    return { supabase };
-}

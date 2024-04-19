cfc97304dccc419f376e734ad003581828e543ca
dakotas17 <dakotas17@nycstudents.net>
2024-04-19 09:22:23 -0400

    agbdikaw

--- vue/src/components/UserAuth.vue
+++ vue/src/components/UserAuth.vue
@@ -20,15 +20,18 @@ const handleSubmit= async () => {
     console.error('Error signing up:', error.message);
   } 
 }
+
 </script>
 
 <template>
   <form @submit.prevent="handleSubmit">
     <h1>Register</h1>
-    <label>Name <input v-model="form.name" type="text" /></label>
-    <label>Email <input v-model="form.email" type="email" /></label>
-    <label>Password <input v-model="form.password" type="password" /></label>
+    <label>Name <input v-model="form.name" type="text" class="form" /></label>
+    <label>Email <input v-model="form.email" type="email" class="form"/></label>
+    <label>Password <input v-model="form.password" type="password" class="form" /></label>
     <button>Register</button>
   </form>
+
+  
 </template>
 

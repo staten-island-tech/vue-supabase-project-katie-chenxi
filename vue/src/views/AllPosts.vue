02cc76d000477c3f9743a3d7ab28f6f5d54afcce
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2024-05-07 09:18:11 -0400

    MAYBE

---
+++ vue/src/views/AllPosts.vue
@@ -0,0 +1,21 @@
+<!-- <template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import{ ref, onMounted } from "vue";
+import {userRoute} from "vue-router"
+let post = ref("");
+const route = useRoute();
+async function getPosts(){
+    let res = await fetch('${route.params.id}')
+    let data = await res.json();
+    post.value = data;
+}
+</script>
+
+<style scoped>
+
+</style> -->

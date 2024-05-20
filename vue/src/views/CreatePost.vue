9f6a596af6e1bf6466008d7ecc29deaf51c788f2
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-05-20 08:45:21 -0400

    a

--- vue/src/views/CreatePost.vue
+++ vue/src/views/CreatePost.vue
@@ -11,17 +11,16 @@
 </template>
 
 <script setup lang="ts">
+import { viewDepthKey } from 'vue-router';
 import { supabase }  from '../supabase.js'
 import { ref } from 'vue'
 
 const title = ref('')
 const description = ref('')
 
-
 const handleSubmit = async () => {
   try {
     const tableName = 'posts';
-/*       const avatarFile = event.target.videos[0] */
     const dataToPost = {
       description: description.value,
       title: title.value,
@@ -31,16 +30,17 @@ const handleSubmit = async () => {
       const { data } = await supabase.from(tableName).upsert([dataToPost])
       console.log('Data posted successfully:', data);
     }
-    /* async function postVideo() {
-      const { data, error } = await supabase
-        .storage
-        .from('avatars')
-        .upload('public/avatar1.png', avatarFile, {
-      cacheControl: '3600',
-      upsert: false
-      })
+    async function postVideo() {
+      const { data } = await supabase
+      .storage
+      .updateBucket('videos' {
+      public: false,
+      allowedMimeTypes: ['image/png'],
+      fileSizeLimit: 1024
+        })
       console.log('Video uploaded successfully', data)
-    } */
+    }
+    postVideo();
     postData();
   }
   catch (error) {

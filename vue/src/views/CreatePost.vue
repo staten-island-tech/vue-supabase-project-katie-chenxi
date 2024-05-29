68942bf9bda30a1443b45c0fa0c77aa844a7a205
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-05-29 09:24:12 -0400

    a

--- vue/src/views/CreatePost.vue
+++ vue/src/views/CreatePost.vue
@@ -3,7 +3,7 @@
     <h1>Create Post</h1>
     <label>Title: <input v-model="title" required type="title" /></label>
     <label>Description: <input v-model="description" required type="description" /></label>
-    <label>Video:<input name="video" type="file" /></label>
+    <label>Video:<input name="video" class="video" type="file" /></label>
     <div>
       <input type="submit" class="button block" />
     </div>
@@ -11,7 +11,7 @@
 </template>
 
 <script setup lang="ts">
-import { supabase }  from '../supabase.js'
+import { supabase } from '../supabase.js'
 import { ref } from 'vue'
 
 const title = ref('')
@@ -31,9 +31,11 @@ const handleSubmit = async () => {
     }
     async function postVideo() {
       const { data } = await supabase
-    .storage
-    .from('videos')
-    .uploadToSignedUrl('folder', 'token-from-createSignedUploadUrl', File)
+        .storage
+        .from('videos')
+        .uploadToSignedUrl('videos', 'token-from-createSignedUploadUrl', File1, {
+          upsert:true,
+        })
       console.log('Video uploaded successfully', data)
     }
     postVideo();
@@ -44,4 +46,3 @@ const handleSubmit = async () => {
   }
 }
 </script>
-

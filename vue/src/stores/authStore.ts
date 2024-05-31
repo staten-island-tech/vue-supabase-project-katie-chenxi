af3ec539ea14eaa517c3289fe19381671e25be76
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2024-05-31 09:03:38 -0400

    Update authStore.ts

--- vue/src/stores/authStore.ts
+++ vue/src/stores/authStore.ts
@@ -15,9 +15,9 @@ export const useAuthStore = defineStore('auth', () => {
       }
  }
  const signUp = async (email, password) => {
-   const { error: signUpError  } = await supabase.auth.signUp({
-     email,
-     password,
+   const { user, error: signUpError  } = await supabase.auth.signUp({
+     email: email.value,
+     password: password.value,
    },
 
    )
@@ -31,8 +31,8 @@ export const useAuthStore = defineStore('auth', () => {
  }
  const signIn = async (email, password) => {
    const { data, error: signInError } = await supabase.auth.signInWithPassword({
-     email,
-     password,
+    email: email.value,
+    password: password.value,
    })
    if (signInError) {
      error.value = signInError.message;

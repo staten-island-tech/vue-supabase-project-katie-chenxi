dfbe0fa3f21776a017fb3d0f585732bac4ce918d
dakotas17 <dakotas17@nycstudents.net>
2024-06-05 09:02:51 -0400

    tdh

--- vue/src/components/UserLogin.vue
+++ vue/src/components/UserLogin.vue
@@ -1,4 +1,34 @@
+<script src="three.r134.min.js"></script>
+<script src="vanta.birds.min.js"></script>
+<script>
+VANTA.BIRDS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
+</script>
 <script setup>
+import BIRDS from 'vanta/dist/vanta.birds.min'
+// Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
+
+export default {
+  mounted() {
+    this.vantaEffect = BIRDS({
+      el: this.$refs.vantaRef
+    })
+  },
+  beforeDestroy() {
+    if (this.vantaEffect) {
+      this.vantaEffect.destroy()
+    }
+  }
+}
+
 import { ref } from 'vue'
 import { supabase } from '../supabase'
 import { useAuthStore } from "@/stores/authStore";
@@ -45,6 +75,9 @@ async function logout() {
 </script>
 
 <template>
+	<div ref='vantaRef'>
+    Foreground content here
+  </div>
   <div v-if="isAuthenticated">User is authenticated!</div>
   <div v-else>Not authenticated.</div>
   <form class="row flex-center flex" @submit.prevent="login">
